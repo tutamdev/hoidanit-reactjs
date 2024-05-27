26e741939b3b9466d599df07c4bd96ee61e2f553
tutamdev <tutamdev@gmail.com>
2024-05-28 06:52:03 +0700

    add MyComponent

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+// class component
+// function component
+import React from "react";
+
+class MyComponent extends React.Component {
+    //JSX
+    render() {
+        return (
+            <div>My first component!</div>
+        );
+    }
+}
+
+export default MyComponent;
